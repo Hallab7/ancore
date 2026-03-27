b6b18c9f8b70cabb1b5a1829fc7bef9f83b506c2
Anioke Sebastian <aniokechukwudi7@gmail.com>
2026-03-27 12:51:33 +0100

    fix: address rebase build fallout

--- apps/extension-wallet/src/background/service-worker.ts
+++ apps/extension-wallet/src/background/service-worker.ts
@@ -1,3 +1,42 @@
+declare namespace chrome {
+  namespace runtime {
+    interface Manifest {
+      name: string;
+      version: string;
+    }
+
+    interface InstalledDetails {
+      reason: string;
+    }
+
+    interface MessageSender {}
+
+    interface OnInstalledEvent {
+      addListener(callback: (details: InstalledDetails) => void): void;
+    }
+
+    interface OnStartupEvent {
+      addListener(callback: () => void): void;
+    }
+
+    interface OnMessageEvent {
+      addListener(
+        callback: (
+          message: unknown,
+          sender: MessageSender,
+          sendResponse: (response: unknown) => void
+        ) => boolean | void
+      ): void;
+    }
+
+    function getManifest(): Manifest;
+
+    const onInstalled: OnInstalledEvent;
+    const onStartup: OnStartupEvent;
+    const onMessage: OnMessageEvent;
+  }
+}
+
 const logPrefix = '[ancore-extension/background]';
 
 const manifest = chrome.runtime.getManifest();
@@ -19,18 +58,24 @@ chrome.runtime.onStartup.addListener(() => {
   console.info(`${logPrefix} startup`);
 });
 
-chrome.runtime.onMessage.addListener((message, _sender, sendResponse) => {
-  const runtimeMessage = message as RuntimeMessage;
+chrome.runtime.onMessage.addListener(
+  (
+    message: unknown,
+    _sender: chrome.runtime.MessageSender,
+    sendResponse: (response: unknown) => void
+  ) => {
+    const runtimeMessage = message as RuntimeMessage;
 
-  if (runtimeMessage.type === 'wallet/ping') {
-    sendResponse({
-      ok: true,
-      version: manifest.version,
-      source: 'service-worker',
-    });
+    if (runtimeMessage.type === 'wallet/ping') {
+      sendResponse({
+        ok: true,
+        version: manifest.version,
+        source: 'service-worker',
+      });
 
-    return true;
+      return true;
+    }
+
+    return false;
   }
-
-  return false;
-});
+);
